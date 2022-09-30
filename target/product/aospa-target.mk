329d16ef1bbaac98e1558462a2ca31d286ca4cbf
Richard Raya <rdxzv.dev@gmail.com>
2022-09-30 13:56:08 -0300

    target: Light debloat
    Change-Id: Ie0fef911f9c7f410192bb4b46bd08e43410b0a1c

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -54,6 +54,10 @@ $(call inherit-product, vendor/aospa/bootanimation/bootanimation.mk)
 PRODUCT_PACKAGES += \
     curl
 
+# Debloater
+PRODUCT_PACKAGES += \
+    Debloater
+
 # Dex2oat
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true
