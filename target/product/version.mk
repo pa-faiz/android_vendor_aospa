7f29a9b7dd106987767488d13c14225b284f00e8
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:34:06 -0500

    signing: support inline signing for U [2/2]
    For those who have used .android-certs previously, cp .android-certs/* vendor/aosp/signing/keys/
    
    Change-Id: If8553fc9f89cbc0923abdae46e42218df14535bf

--- target/product/version.mk
+++ target/product/version.mk
@@ -75,11 +75,11 @@ AOSPA_VERSION := $(shell echo -n $(AOSPA_VERSION) | tr -d '[:space:]')
 
 # Signing
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+ifneq (,$(wildcard vendor/aospa/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aospa/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/aospa/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/aospa/signing/keys/otakey.x509.pem
 endif
 endif
