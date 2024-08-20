f879256070100050308c1682bdab8a1cbcd6ad47
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-20 08:50:50 +0530

    sepolicy: Add support for custom freeform service
    Change-Id: I308176f8b186ecf365e4b98ac792c2f5e9872335

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -31,6 +31,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     Glimpse
 
+# LMOFreeform
+PRODUCT_PACKAGES += \
+    LMOFreeform \
+    LMOFreeformSidebar
+
 # DesktopMode
 PRODUCT_PACKAGES += \
     DesktopMode
