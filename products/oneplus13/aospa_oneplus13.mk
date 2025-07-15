b4f3301a2b1d1eec132a71aeebaf2a059dcc5a99
Fazil Sheik <fazilsheik@aospa.co>
2025-07-15 20:28:18 +0530

    products: Introduce OnePlus 13 (dodge)
    Change-Id: I83d3ae6bdcda8ca8ee6ef25088f48af07472d278
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

---
+++ products/oneplus13/aospa_oneplus13.mk
@@ -0,0 +1,37 @@
+#
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+
+# Check for target product
+ifeq (aospa_oneplus13,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/oneplus/oneplus13/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := aospa_oneplus13
+PRODUCT_DEVICE := oneplus13
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2653
+PRODUCT_MANUFACTURER := OnePlus
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_SYSTEM_NAME := OnePlus13
+PRODUCT_SYSTEM_DEVICE := OnePlus13
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+endif
