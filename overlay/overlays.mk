09fc87e7fb13cce3a249684d22546ac3614173c1
Michael Bestas <mkbestas@lineageos.org>
2025-12-16 15:49:26 +0900

    overlay: Import extra color bundles from TQ2A.230305.008.C1
    * Extracted from cheetah TQ2A.230305.008.C1 with the following:
      - adb shell cmd overlay dump com.android.systemui:accent
      - adb shell cmd overlay dump com.android.systemui:neutral
    
    Change-Id: Ic039fa96e11608f04af78390fb03370794f7f08f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -58,7 +58,8 @@ PRODUCT_PACKAGES += \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay \
-    ParanoidLauncherOverlay
+    ParanoidLauncherOverlay \
+    PixelThemesStub
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/static
 PRODUCT_PACKAGE_OVERLAYS += vendor/aospa/overlay/static
