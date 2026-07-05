39a7549d563be027ce70c1d19072a4bbc1f92284
Omkar Chandorkar <gotenksIN@aospa.co>
2026-07-05 09:03:04 +0530

    aospa: Upgrade to Calcite
    Change-Id: I10c60a5c28df577b35bd131e5a3098e7ff23bcb1
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- target/product/version.mk
+++ target/product/version.mk
@@ -15,7 +15,7 @@
 # This is the global AOSPA version flavor that determines the focal point
 # behind our releases. This is bundled alongside $(AOSPA_MINOR_VERSION)
 # and only changes per major Android releases.
-AOSPA_MAJOR_VERSION := beryl
+AOSPA_MAJOR_VERSION := calcite
 
 # The version code is the upgradable portion during the cycle of
 # every major Android release. Each version code upgrade indicates
